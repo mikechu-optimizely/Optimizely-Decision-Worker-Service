c10a34b7725fd693adfff929b28984374c9675d1
Mike Chu <michael.chu@optimizely.com>
2024-11-08 10:00:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+using Optimizely_Decision_Worker_Service;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
